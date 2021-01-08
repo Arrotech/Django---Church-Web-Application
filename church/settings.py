9a728a2d774f61620e3b2e5777d60b437fb47a26
Harun Gachanja <arrotechdesign@gmail.com>
2021-01-08 16:27:43 +0300

    install django-heroku and gunicorn

--- church/settings.py
+++ church/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
+
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -24,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['djangochurchwebapp.herokuapp.com']
+ALLOWED_HOSTS = ['eternityglorychurch.herokuapp.com']
 
 
 # Application definition
